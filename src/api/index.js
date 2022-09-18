47cebef15f434af0f0ca24e108fdd030f152074c
daraksha <daraksha.atg@gmail.com>
2022-09-18 13:50:18 +0530

    updated

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://memories-server-dtg.herokuapp.com/",
+});
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+
+export const signIn = (formData) => API.post("/user/signin", formData);
+export const signUp = (formData) => API.post("/user/signup", formData);
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import posts from "../reducers/posts";
+import auth from "../reducers/auth";
+
+export default combineReducers({ posts, auth });
